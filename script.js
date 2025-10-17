91d0188d8674db6da35c810c344de13bad1884d4
ta2389933-eng <ta2389933@gmail.com>
2025-10-17 10:11:25 +0500

    BEST FIGMA FOOD BLOG WEBSITE

---
+++ script.js
@@ -0,0 +1,26 @@
+// let a = 1, b = 3, c = 20, d = 100;
+
+
+// let ary = [12, 1.5, 'tahir', 'Mujahid', true, false, 12.6573];
+
+// console.log(ary);
+
+
+// console.log(ary[3]);
+
+// console.log(ary[6]);
+
+
+// console.log(ary[2]);
+
+// console.log(ary[0]);
+
+// console.log(ary[1]);
+
+
+
+const array = ['Tahir Ali', [false, true, 15, 6562, 662, 2772], 'moenuddin', ['he is my Friend', 726, 15, 62, 71, 828]];
+
+console.log(array);
+
+console.log(array[3][0]);//array ke ander jo array hai us ko 
